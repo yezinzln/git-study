0b5469b684d93471e23935e5bda28da579aee9ff
yezlnzln <yejin2791@gmail.com>
2020-07-02 15:45:15 +0900

    new c exam file [ add ]

---
+++ ex.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int sum(int v1, int v2);
+
+int main(int argc, char* argv[]){
+	int result;
+	result = sum (10,20);		
+	printf("result = %d", result);
+	return 0;
+}
+
+int sum(int v1, int v2){
+	return v1+v2;
+}
